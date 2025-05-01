971bd8b490e24f250214eb115b3d5e7211ae6b3e
Hojiakbar Habibullayev <hojiakbarhabibullayev@Hojiakbarui-MacBookPro.local>
2025-05-01 21:17:39 +0900

    HTTP methods 2

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ import express from 'express'
 const app = express()
 const port = 8080
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
-	res.send('hello world')
+	res.status(200).json({ message: 'hello world' })
+})
+app.post('/', (req, res) => {
+	const { firstName, lastName } = req.body
+	const msg = `his fullname is - ${firstName} ${lastName}`
+
+	res.send(msg)
+})
+
+app.delete('/:id', (req, res) => {
+	const { id } = req.params
+	res.send(id)
 })
 
 app.listen(port, () =>
